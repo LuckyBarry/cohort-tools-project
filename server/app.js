6d02276603dfd3f951b21c71168c31d64e334cdb
Jos Bakker <josbakkeramsterdam@gmail.com>
2023-10-16 16:37:09 +0200

    cors installed

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
+const cors = require("cors");
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
@@ -18,11 +20,13 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(cors({ origin: ["http://localhost:5005/", "http://localhost:5005/api/cohorts", "http://localhost:5005/api/students"] }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
