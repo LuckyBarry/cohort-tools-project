34b3b0a9b035ef089b0b062b28fae30bcc7ff77a
sushma subramaniyan <sushmasubramaniyan14@gmail.com>
2023-10-18 15:01:47 +0200

    working

--- server/models/Cohort.model.js
+++ server/models/Cohort.model.js
@@ -14,5 +14,39 @@ const cohortSchema = new Schema({
     leadTeacher: String,
     totalHours: Number
 });
+app.post('/api/cohorts', async (request, response) => {
+    try {
+      const newCohort = await Cohort.create(request.body)
+      response.status(201).json({ cohort: newCohort })
+    } catch (error) {
+      console.log(error)
+      response.status(400).json({ error })
+    }
+  })
+  app.get('/api/cohorts',async(req,res)=>{
+    try{
+      const allCohorts =await Cohort.find();
+          response.status(201).json(allCohorts);
+      }catch (error) {
+          res.status(500).json({message: "not created"});
+          
+      }
+  });
+  app.get('/api/cohorts/:cohortId',async(req,res)=>{
+    const {_id} = request.params
+    console.log(_id)
+
+    try{
+      const oneCohort =await Cohort.findById(_id,request.body);
+          response.status(201).json({cohort: oneCohort})
+      }catch (error) {
+          res.status(500).json({message: "not created"});
+          
+      }
+  });
+
+
+
+
 const Cohort = mongoose.model("Cohort", cohortSchema)
 module.exports = Cohort;
--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema("student",studentSchema);
 const studentSchema = new Schema({
     _id: Number,
     firstName: String,
@@ -14,5 +14,37 @@ const studentSchema = new Schema({
     cohort: Number,
     projects: Array
 });
+
+app.get('/api/students',async(req,res)=>{
+   try{
+     const students =await Student.find();
+         response.status(201).json(students);
+     }catch (error) {
+         res.status(500).json({message: "not created"});
+         
+     }
+ });
+
+ app.post('/api/students', async (request, response) => {
+    try {
+      const newStudent = await Student.create(request.body)
+      response.status(201).json({ student: newStudent })
+    } catch (error) {
+      console.log(error)
+      response.status(400).json({ error })
+    }
+  })
+  app.get('/api/students',async(req,res)=>{
+    console.log(request.query)
+    const {}
+    try {
+        const allData = await Student.find({})
+        response.json({allData})
+    } catch (error) {
+        response.
+    }
+
+  })
+ 
 const Student = mongoose.model("Student", studentSchema)
 module.exports = Student;
